7b3aebcbfdfa27fc4d4711d4c15f66dc7cf492a0
Sascha Manns <Sascha.Manns@outlook.de>
2022-12-31 10:14:36 +0100

    Initial commit

---
+++ JobApplicationManager/ViewModels/ListDetailsViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.ObjectModel;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+using JobApplicationManager.Contracts.ViewModels;
+using JobApplicationManager.Core.Contracts.Services;
+using JobApplicationManager.Core.Models;
+
+namespace JobApplicationManager.ViewModels;
+
+public class ListDetailsViewModel : ObservableRecipient, INavigationAware
+{
+    private readonly ISampleDataService _sampleDataService;
+    private SampleOrder? _selected;
+
+    public SampleOrder? Selected
+    {
+        get => _selected;
+        set => SetProperty(ref _selected, value);
+    }
+
+    public ObservableCollection<SampleOrder> SampleItems { get; private set; } = new ObservableCollection<SampleOrder>();
+
+    public ListDetailsViewModel(ISampleDataService sampleDataService)
+    {
+        _sampleDataService = sampleDataService;
+    }
+
+    public async void OnNavigatedTo(object parameter)
+    {
+        SampleItems.Clear();
+
+        // TODO: Replace with real data.
+        var data = await _sampleDataService.GetListDetailsDataAsync();
+
+        foreach (var item in data)
+        {
+            SampleItems.Add(item);
+        }
+    }
+
+    public void OnNavigatedFrom()
+    {
+    }
+
+    public void EnsureItemSelected()
+    {
+        if (Selected == null)
+        {
+            Selected = SampleItems.First();
+        }
+    }
+}
---
+++ JobApplicationManager/Views/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using JobApplicationManager.ViewModels;
+
+using Microsoft.UI.Xaml.Controls;
+
+namespace JobApplicationManager.Views;
+
+public sealed partial class MainPage : Page
+{
+    public MainViewModel ViewModel
+    {
+        get;
+    }
+
+    public MainPage()
+    {
+        ViewModel = App.GetService<MainViewModel>();
+        InitializeComponent();
+    }
+}
